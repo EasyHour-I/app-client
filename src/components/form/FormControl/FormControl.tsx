cc341d1f60619a20601fbb94103ffbf0e426a156
Yonatan Eshed <yonatan.eshed@gmail.com>
2024-03-10 18:56:29 +0200

    feat: impolement FormControl component

---
+++ src/components/form/FormControl/FormControl.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren } from 'react';
+import { FieldErrors } from 'react-hook-form';
+import styles from './FormControl.module.scss';
+import cn from 'classnames';
+
+export interface FormControlProps {
+    name: string;
+    label: string;
+    errors?: FieldErrors;
+    required?: boolean;
+    className?: string;
+}
+
+export const FormControl: React.FC<PropsWithChildren<FormControlProps>> = ({
+    label,
+    required,
+    name,
+    children,
+    errors,
+    className,
+}) => {
+    return (
+        <div className={cn(styles.container, className)}>
+            <label className={required ? styles.required : ''}>{label}</label>
+            {children}
+            {errors?.[name] && <span>{errors[name]?.message as string}</span>}
+            <span>Error text</span>
+        </div>
+    );
+};
